d6c32072afdebf44b44ec24dea1c5ac3c697b6a2
Naveen Giri <1naveengiri@gmail.com>
2024-12-25 19:12:00 +0530

    📌 Add setting page display content

--- admin/partials/geobuddy-admin-display.php
+++ admin/partials/geobuddy-admin-display.php
@@ -1,16 +1,38 @@
 <?php
-
 /**
  * Provide a admin area view for the plugin
  *
- * This file is used to markup the admin-facing aspects of the plugin.
- *
- * @link       https://https://buddydevelopers.com
+ * @link       https://buddydevelopers.com
  * @since      1.0.0
  *
  * @package    Geobuddy
  * @subpackage Geobuddy/admin/partials
  */
+
+$active_tab = isset($_GET['tab']) ? $_GET['tab'] : 'general';
 ?>
 
-<!-- This file should primarily consist of HTML with a little bit of PHP. -->
+<div class="wrap">
+    <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
+
+    <h2 class="nav-tab-wrapper">
+        <a href="?page=geobuddy&tab=general" class="nav-tab <?php echo $active_tab == 'general' ? 'nav-tab-active' : ''; ?>">
+            <?php _e('General', 'geobuddy'); ?>
+        </a>
+        <a href="?page=geobuddy&tab=map" class="nav-tab <?php echo $active_tab == 'map' ? 'nav-tab-active' : ''; ?>">
+            <?php _e('Map', 'geobuddy'); ?>
+        </a>
+    </h2>
+
+    <div class="tab-content">
+        <?php if ($active_tab == 'general'): ?>
+            <div class="general-settings">
+                <p>General Settings Content</p>
+            </div>
+        <?php else: ?>
+            <div class="map-settings">
+                <p>Map Settings Content</p>
+            </div>
+        <?php endif; ?>
+    </div>
+</div>
